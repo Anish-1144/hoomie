c0c13b8f845586e763f5b4a537519e3f55d0d576
Anish-1144 <dev.anishmourya27@gmail.com>
2026-04-17 17:49:41 +0530

    Add Facebook Pixel tracking script to RootLayout for enhanced analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Caveat, Inter, Poppins } from "next/font/google";
+import Script from "next/script";
 import "../styles/globals.css";
 
 const inter = Inter({
@@ -30,7 +31,32 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={`${inter.variable} ${poppins.variable} ${caveat.variable}`}>{children}</body>
+      <body className={`${inter.variable} ${poppins.variable} ${caveat.variable}`}>
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '2154105088756991');
+fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img
+            height={1}
+            width={1}
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=2154105088756991&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+        {children}
+      </body>
     </html>
   );
 }
